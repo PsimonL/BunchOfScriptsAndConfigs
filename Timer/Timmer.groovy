8c715e38cb4677648c5e5a41c808bbd9e4abea4d
Szymon Rogowski <szymon0r0official@gmail.com>
2023-06-12 20:43:58 +0200

    timer

---
+++ Timer/Timmer.groovy
@@ -0,0 +1,33 @@
+import java.time.Duration
+
+static void main(String[] args) {
+    println "Hello world!"
+    def startTime = System.currentTimeMillis()
+
+    def timer = new Timer()
+    timer.schedule(new TimerTask() {
+        def count = 0
+
+        @Override
+        void run() {
+            count++
+            println "Time: ${count} s"
+        }
+    }, 1000, 1000)
+
+    System.in.withReader { reader ->
+        reader.readLine()
+    }
+
+    timer.cancel()
+
+    def endTime = System.currentTimeMillis()
+    def totalTime = Duration.ofMillis(endTime - startTime)
+
+    def days = totalTime.toDaysPart()
+    def hours = totalTime.toHoursPart()
+    def minutes = totalTime.toMinutesPart()
+    def seconds = totalTime.toSecondsPart()
+
+    println "Total time: ${seconds} s, ${minutes} min, ${hours} h, ${days} d"
+}
