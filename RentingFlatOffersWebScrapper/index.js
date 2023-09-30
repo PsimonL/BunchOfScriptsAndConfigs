8313cb233d649d0435cb7fdd4f4c3670a1dede37
Szymon Rogowski <szymon0r0official@gmail.com>
2023-09-30 12:20:34 +0200

    web scrapper

---
+++ RentingFlatOffersWebScrapper/index.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+
+const url = 'https://example.com'; 
+
+async function fetchPage(url) {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.error('Błąd podczas pobierania strony:', error);
+    throw error;
+  }
+}
+
+async function main() {
+  try {
+    const html = await fetchPage(url);
+    console.log('Zawartość strony:');
+    console.log(html);
+  } catch (error) {
+    console.error('Błąd głównej funkcji:', error);
+  }
+}
+
+main();
